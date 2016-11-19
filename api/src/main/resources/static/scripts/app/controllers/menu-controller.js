0f4500ca593f18cb382911c9e3f814480c8b129c
Bajtas <bajtas@gmail.com>
2016-11-19 22:46:43 +0100

    Missing files added. Minor changes in dashboard.

--- api/src/main/resources/static/scripts/app/controllers/menu-controller.js
+++ api/src/main/resources/static/scripts/app/controllers/menu-controller.js
@@ -1,6 +1,6 @@
 angular.module('SquareMooseControllers')
 
 .controller('MenuCtrl', function($scope, $rootScope, $http, $state, $base64, $location) {
+   $scope.location = $location;
    $scope.showMenu =  !$location.path().includes('login');
-
 });
